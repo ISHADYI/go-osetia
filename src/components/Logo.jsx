0a107423a1e93ab2c4b51114a00e2426be403c4b
ISHADYI <shadyworkq@gmail.com>
2026-04-23 10:22:35 +0300

    Начало начал

---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+export function Logo() {
+    return (
+        <a href="#" className="flex items-center gap-1">
+            <img src="/logo.svg" alt="" />
+            <span className="font-semibold size-5">GoOsetia</span>
+        </a>
+    )
+}
